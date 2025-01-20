8ac4f7aebfb8a9718309a0ec06ce8564a8c89eec
mohammedkmo <alrasamking123@gmail.com>
2025-01-20 10:42:18 +0300

    disable eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,23 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.config({
+    extends: ['next'],
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+      'react/no-unknown-property': 'off',
+      'react/prop-types': 'off',
+      'react/jsx-no-target-blank': 'off',
+      'react/jsx-no-comment-textnodes': 'off',
+      'react/jsx-no-useless-fragment': 'off',
+      'react/jsx-no-duplicate-props': 'off',
+      'react/jsx-no-script-url': 'off',
+      'react/jsx-no-target-blank': 'off',
+      'react/jsx-no-useless-fragment': 'off',
+      'react/jsx-no-script-url': 'off',
+    },
+  }),
+]
 
 export default eslintConfig;
